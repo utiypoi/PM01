e2e892b2847e1081e95f61622fa484dca73ff381
utiypoi <ut26iy00@gmail.com>
2023-04-19 14:03:26 +0500

    end

--- app/src/main/java/com/example/trainingapp/OrganizationActivity.java
+++ app/src/main/java/com/example/trainingapp/OrganizationActivity.java
@@ -16,6 +16,10 @@ import java.util.ArrayList;
 public class OrganizationActivity extends AppCompatActivity {
     ArrayList<Organization> organizations = new ArrayList<Organization>();
     ListView organizationList;
+    EditText nameOrganization;
+    EditText shortNameOrganization;
+    EditText addressOrganization;
+    String name, shortName, address;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,35 +27,56 @@ public class OrganizationActivity extends AppCompatActivity {
         setContentView(R.layout.activity_organization);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         setInitialData();
-        Init();
+        nameOrganization = findViewById(R.id.nameOrganization);
+        shortNameOrganization = findViewById(R.id.shortNameOrganization);
+        addressOrganization = findViewById(R.id.addressOrganization);
         organizationList = findViewById(R.id.listViewOrganization);
-        OrganizationAdapter organizationAdapter = new OrganizationAdapter(this, R.layout.list_item,organizations);
+        OrganizationAdapter organizationAdapter = new OrganizationAdapter(this, R.layout.list_item, organizations);
         organizationList.setAdapter(organizationAdapter);
         AdapterView.OnItemClickListener itemClickListener = new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Organization selectedOrganization = (Organization) parent.getItemAtPosition(position);
-                Toast.makeText(getApplicationContext(), "Выбрано: " + selectedOrganization.getId(), Toast.LENGTH_SHORT).show();
+                name = selectedOrganization.getName();
+                shortName = selectedOrganization.getShort_name();
+                address = selectedOrganization.getAddress();
+                nameOrganization.setText(name);
+                shortNameOrganization.setText(shortName);
+                addressOrganization.setText(address);
+//                Toast.makeText(getApplicationContext(), "Выбрано: " + selectedOrganization.getId(), Toast.LENGTH_SHORT).show();
             }
         };
         organizationList.setOnItemClickListener(itemClickListener);
     }
 
-    private void Init() {
-        EditText nameOrganization = (EditText) findViewById(R.id.nameOrganization);
-        EditText shortNameOrganization = (EditText) findViewById(R.id.shortNameOrganization);
-        EditText addressOrganization = (EditText) findViewById(R.id.addressOrganization);
-    }
+    int n = 0;
 
     private void setInitialData() {
-        organizations.add(new Organization(1,
+        organizations.add(new Organization(n + 1,
                 "Учреждение повышения квалификации и переподготовки технических специальностей 'ItTech'",
                 "ItTech",
                 "ул. Гоголя, д. 223"));
 
-        organizations.add(new Organization(2,
+        organizations.add(new Organization(n + 2,
                 "ГАПОУ 'Оренбургский колледж экономики и информатики'",
                 "ГАПОУ 'ОКЭИ'",
                 "ул. Чкалова, д. 11"));
+    }
+
+    public void saveEntry(View view) {
+        //пример
+        if (nameOrganization.length() == 0) {
+            Toast.makeText(this, "Введите наименование организации", Toast.LENGTH_SHORT).show();
+        } else if (shortNameOrganization.length() == 0) {
+            Toast.makeText(this, "Введите краткое наименование организации", Toast.LENGTH_SHORT).show();
+        } else if (addressOrganization.length() == 0) {
+            Toast.makeText(this, "Введите адрес организации", Toast.LENGTH_SHORT).show();
+        } else if (nameOrganization.length() == 0 && shortNameOrganization.length() == 0 && addressOrganization.length() == 0) {
+            Toast.makeText(this, "Все поля должны быть заполнены", Toast.LENGTH_SHORT).show();
+        } else {
+            organizations.add(new Organization(n + 3, nameOrganization.getText().toString(), shortNameOrganization.getText().toString(), addressOrganization.getText().toString()));
+            OrganizationAdapter organizationAdapter = new OrganizationAdapter(this, R.layout.list_item, organizations);
+            organizationList.setAdapter(organizationAdapter);
+        }
     }
 }
